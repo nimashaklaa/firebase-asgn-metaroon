58c3d61dd79b347ad6273633d23205e5b25f3794
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2024-01-23 20:33:02 +0530

    feat: update the user profile

--- firebase-assignment/src/screens/UserProfile.tsx
+++ firebase-assignment/src/screens/UserProfile.tsx
@@ -1,12 +1,61 @@
-import {useState} from "react";
+import {useState, useEffect } from "react";
+import {useNavigate} from "react-router-dom";
+import { auth, db } from '../config/firebase-config.ts';
+import {doc,setDoc,getDoc} from 'firebase/firestore'
+import './UserProfile.css'
 
 const UserProfile = ()=>{
     const [fname ,setFname] = useState('');
     const [lname, setLname] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Fetch user data from Firestore if available
+        const fetchUserData = async () => {
+            try {
+                if(auth.currentUser){
+                    const userDoc = doc(db, 'users', auth.currentUser.uid);
+                    const userDocSnap = await getDoc(userDoc);
+
+                    if (userDocSnap.exists()) {
+                        const userData = userDocSnap.data();
+                        setFname(userData.fname || '');
+                        setLname(userData.lname || '');
+                    }
+                }
+                else{
+                    console.error("User is not authenticated");
+                }
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+            }
+        };
+        fetchUserData();
+    }, []); // Run the effect only once, on component mount
+
+    const saveUserData = async () => {
+        try {
+            if(auth.currentUser){
+                // Save user data to Firestore
+                const userDocRef = doc(db, 'users', auth.currentUser.uid);
+                await setDoc(userDocRef, { fname, lname });
+
+                console.log("User data saved successfully");
+
+                navigate("/profile");
+            }
+            else{
+                console.error("User is not authenticated");
+            }
+        } catch (error) {
+            console.error("Error saving user data:", error);
+        }
+    };
+
     return(
         <div className="userProfile-container">
             <h1>Welcome to Webpage</h1>
-            <div className="userpass-container">
+            <div >
                 <div className="input-field">
                     <label htmlFor="fname">First Name:</label>
                     <input
